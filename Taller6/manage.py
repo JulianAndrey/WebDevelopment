494e6482bd497a663d4616f7af81de33e4a86387
JulianAndrey <julianandreyrodrirome@gmail.com>
2022-08-13 21:50:15 -0500

    Update manage.py

--- Taller6/manage.py
+++ Taller6/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taller_6.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taller6.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,3 +20,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
